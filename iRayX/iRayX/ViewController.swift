74aed3a6b79c280a86246f248e7e33186b438d1a
Ren.Yi <ren.yi@atlasp.io>
2019-07-17 14:10:15 +0800

    init

---
+++ iRayX/iRayX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iRayX
+//
+//  Created by yiren on 2019/7/17.
+//  Copyright © 2019 yiren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
